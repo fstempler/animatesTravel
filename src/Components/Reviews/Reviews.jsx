81aa56dcce4bc31fcfbed57ee42da5ecfa17771c
fstempler <zackmango@gmail.com>
2024-04-02 16:32:05 -0300

    Add Client Review

--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -5,6 +5,11 @@ import profile1 from '../../assets/reviews/Profile1.jpg';
 import profile2 from '../../assets/reviews/Profile2.jpg';
 import profile3 from '../../assets/reviews/Profile3.jpg';
 
+//Review1
+import review11 from '../../assets/reviews/Review 1-1.png';
+import review12 from '../../assets/reviews/Review 1-2.png';
+import review13 from '../../assets/reviews/Review 1-3.png';
+
 import './reviews.css'
 
 const Reviews = () => {
@@ -20,15 +25,17 @@ const Reviews = () => {
                 <div className='row rowHandle'>
                     <div className='col-sm-4 col-lg-3 col-xl-3 col-xxl-3 reviews__Card-Item'>
                         <ReviewCard 
-                        imageUrl={profile1}
-                        title={"Juan Ignacio Pirrota"}
-                        text={"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quaerat totam dignissimos hic, harum dolorum consequatur corrupti doloribus alias necessitatibus tempore fugit officiis quos sunt numquam in magni minima quisquam!"}                        
+                        imageUrl1={review11}
+                        imageUrl2={review12}
+                        imageUrl3={review13}
+                        title={"Natalia Fuccini"}
+                        text={"Migrar en familia nunca es sencillo y encontrar quién nos pudiera asesorar y ayudar al traslado de nuestra familia de cuatro patas tampoco. No conocimos personalmente a los ángeles detrás de Animates Travel hasta el momento del viaje, pero la tranquilidad y el profesionalismo que transmitieron desde la primera llamada es indescriptible. El viaje salió todo de 10 y llegaron a nuestro nuevo hogar con toda la documentación para poder ser registrados. Los recomendamos siempre que podemos. Calidad y calidez cinco estrellas!"}                        
                         animation={'flip-up'}
                         />
                     </div>
                     <div className='col-sm-4 col-lg-3 col-xl-3 col-xxl-3 reviews__Card-Item'>
                         <ReviewCard 
-                        imageUrl={profile2}
+                        imageUrl1={profile2}
                         title={"María y Paz"}
                         text={"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quaerat totam dignissimos hic, harum dolorum consequatur corrupti doloribus alias necessitatibus tempore fugit officiis quos sunt numquam in magni minima quisquam!"}                        
                         animation={'flip-up'}
@@ -36,7 +43,7 @@ const Reviews = () => {
                     </div>
                     <div className='col-sm-4 col-lg-3 col-xl-3 col-xxl-3 reviews__Card-Item'>
                         <ReviewCard 
-                        imageUrl={profile3}
+                        imageUrl1={profile3}
                         title={"Sofía Andressi"}
                         text={"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quaerat totam dignissimos hic, harum dolorum consequatur corrupti doloribus alias necessitatibus tempore fugit officiis quos sunt numquam in magni minima quisquam!"}                        
                         animation={'flip-up'}
--- src/Components/Reviews/components/ReviewCard.jsx
+++ src/Components/Reviews/components/ReviewCard.jsx
@@ -4,7 +4,7 @@ import 'aos/dist/aos.css'
 
 import { useEffect } from 'react';
 
-const ReviewCard = ({ imageUrl, title, text, animation }) => {
+const ReviewCard = ({ imageUrl1, imageUrl2, imageUrl3, title, text, animation }) => {
 
     useEffect(() => {
         Aos.init({duration: 1500})
@@ -12,7 +12,26 @@ const ReviewCard = ({ imageUrl, title, text, animation }) => {
 
     return (
         <div className='reviewCard__Container' data-aos={animation}>
-            <img src={imageUrl} className='reviewCard__Img' />
+            {/* <img src={imageUrl} className='reviewCard__Img' /> */}
+            <div id="carouselExample" className="carousel slide">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src={imageUrl1} className="d-block w-100 reviewCard__Img" alt="..."/>
+                    </div>
+                    <div className="carousel-item">
+                        <img src={imageUrl2} className="d-block w-100 reviewCard__Img" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src={imageUrl3} className="d-block w-100 reviewCard__Img" alt="..." />
+                    </div>
+                </div>
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>                        
+                    </button>
+                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>                        
+                    </button>
+            </div>
             <div className='reviewCard__Title-Container'>
                 <h4 className='reviewCard__Title'>{title}</h4>
             </div>
